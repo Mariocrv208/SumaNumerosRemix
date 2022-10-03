6ba891132119dd06e97215ea9ccf302f8a84e8d5
Remix Workspace <>
2022-10-03 02:17:26 -0600

    Commit Sumando Numeros

---
+++ contracts/suma.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0; //Version de solidity
+
+contract Suma{
+    function Sumando(int num1, int num2) public pure returns(int){
+        return num1 + num2;
+    }
+}
